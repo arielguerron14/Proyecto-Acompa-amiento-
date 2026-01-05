d21a3b7dcf31808c837bba77d0afad7f91eb2cf2
Ariel Guerrón <caguerronp@uce.edu.ec>
2026-01-05 11:59:41 -0500

    Fix: Update all microservice database IPs to 13.220.99.207

--- micro-reportes-estudiantes/src/config/index.js
+++ micro-reportes-estudiantes/src/config/index.js
@@ -3,7 +3,7 @@ dotenv.config();
 
 module.exports = {
   PORT: process.env.PORT || 5003,
-  MONGO_URI: process.env.MONGO_URI || process.env.MONGO_URL || `mongodb://${process.env.DB_HOST || '98.84.26.109'}:${process.env.DB_PORT || 27017}/reportesestdb`,
+  MONGO_URI: process.env.MONGO_URI || process.env.MONGO_URL || `mongodb://${process.env.DB_HOST || '13.220.99.207'}:${process.env.DB_PORT || 27017}/reportesestdb`,
   RATE_LIMIT_WINDOW_MS: process.env.RATE_LIMIT_WINDOW_MS ? Number(process.env.RATE_LIMIT_WINDOW_MS) : 15 * 60 * 1000,
   RATE_LIMIT_MAX: process.env.RATE_LIMIT_MAX ? Number(process.env.RATE_LIMIT_MAX) : 100,
   CORS_WHITELIST: process.env.CORS_WHITELIST ? process.env.CORS_WHITELIST.split(',') : ['http://localhost:5500','http://localhost:8080'],
