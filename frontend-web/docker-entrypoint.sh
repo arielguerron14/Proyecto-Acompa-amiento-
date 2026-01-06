7d475a011cbb799707b998779b14ac32ac3d4c22
Ariel Guerrón <caguerronp@uce.edu.ec>
2026-01-06 15:00:37 -0500

    fix: use sed instead of envsubst

--- frontend-web/docker-entrypoint.sh
+++ frontend-web/docker-entrypoint.sh
@@ -4,8 +4,8 @@ set -e
 # Use provided API_GATEWAY_HOST or default
 API_GATEWAY_HOST="${API_GATEWAY_HOST:-172.31.78.183}"
 
-# Substitute environment variables in the nginx config template
-envsubst < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+# Substitute environment variables in the nginx config template using sed
+sed "s|\${API_GATEWAY_HOST}|$API_GATEWAY_HOST|g" /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
 # Start nginx
 exec nginx -g "daemon off;"
