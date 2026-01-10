49143353d1a6b909594b39b519f4f464c0f9d8c0
Ariel Guerrón <caguerronp@uce.edu.ec>
2026-01-09 19:22:01 -0500

    debug(config): add env var logging to diagnose service URL loading

--- api-gateway/src/config.js
+++ api-gateway/src/config.js
@@ -1,7 +1,15 @@
-console.log('[DEBUG] AUTH_URL:', process.env.AUTH_URL, '| AUTH_SERVICE:', process.env.AUTH_SERVICE);
+console.log('[CONFIG DEBUG] AUTH_URL env:', process.env.AUTH_URL);
+console.log('[CONFIG DEBUG] AUTH_SERVICE env:', process.env.AUTH_SERVICE);
+console.log('[CONFIG DEBUG] ESTUDIANTES_SERVICE env:', process.env.ESTUDIANTES_SERVICE);
+console.log('[CONFIG DEBUG] MAESTROS_SERVICE env:', process.env.MAESTROS_SERVICE);
+
 module.exports = {
   AUTH_SERVICE: process.env.AUTH_URL || process.env.AUTH_SERVICE || 'http://localhost:3000',
   ESTUDIANTES_SERVICE: process.env.ESTUDIANTES_URL || process.env.ESTUDIANTES_SERVICE || 'http://micro-estudiantes:3002',
   MAESTROS_SERVICE: process.env.MAESTROS_URL || process.env.MAESTROS_SERVICE || 'http://micro-maestros:5001',
   PORT: process.env.PORT || 8080,
 };
+
+console.log('[CONFIG DEBUG] RESOLVED AUTH_SERVICE:', module.exports.AUTH_SERVICE);
+console.log('[CONFIG DEBUG] RESOLVED ESTUDIANTES_SERVICE:', module.exports.ESTUDIANTES_SERVICE);
+console.log('[CONFIG DEBUG] RESOLVED MAESTROS_SERVICE:', module.exports.MAESTROS_SERVICE);
